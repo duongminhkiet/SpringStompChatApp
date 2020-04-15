f7deee269067fd84bddbdbe02efa38d3eed051d2
kietdm <kietdm@cpc.vn>
2020-04-15 18:06:14 +0700

    init project

---
+++ src/main/java/com/zmk/spring/stomp/chatapp/SpringStompChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.zmk.spring.stomp.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStompChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStompChatAppApplication.class, args);
+	}
+
+}
